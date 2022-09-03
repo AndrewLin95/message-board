1e7c0c211d8b8378956a89e90435e76cbd07ef39
AndrewLin95 <andrew.lin88@hotmail.com>
2022-09-03 17:03:54 -0400

    added api request to node server to fetch api data to display onto home page

--- client/src/Components/Pages/Home.tsx
+++ client/src/Components/Pages/Home.tsx
@@ -1,16 +1,37 @@
-import {FC} from 'react';
+import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+interface Data {
+  text: string;
+  user: string;
+  added: string;
+}
+
 const Home: FC = () => {
+  const [data, setData] = useState<Data[]>([]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data])
 
   return(
     <>
       <div className='messageContainer'>
-        <div className='messageCard'>
-        <div className='text'>Hi There</div>
-        <div className='user'>Amando</div>
-        <div className='date'>date</div>
-        </div>
+        {Object.entries(data).map(([key, value]) => {
+          return (
+            <div className='messageCard' key={key}>
+              <div className='text'>{value.text}</div>
+              <div className='user'>{value.user}</div>
+              <div className='date'>{value.added}</div>
+            </div>
+          )
+      })}
       </div>
       <Link id='newMessage' to="/new">New Message</Link>
     </>
